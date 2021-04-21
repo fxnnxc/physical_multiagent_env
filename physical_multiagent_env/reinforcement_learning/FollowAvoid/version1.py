d0e00328b8d64965f704016c6582812dfde650a5
fxnnxc <beomjin26@naver.com>
2021-04-21 15:56:42 +0900

    [Experiment] Finished curriculum learning

--- physical_multiagent_env/reinforcement_learning/FollowAvoid/version1.py
+++ physical_multiagent_env/reinforcement_learning/FollowAvoid/version1.py
@@ -2,6 +2,7 @@ import json
 import argparse 
 import pybullet as p 
 import time 
+import numpy as np 
 
 import ray 
 from ray.tune.registry import register_env 
@@ -12,6 +13,7 @@ from ray.rllib.models import ModelCatalog
 from gym.spaces import Discrete, Box, Dict 
 from ray.rllib.env.multi_agent_env import MultiAgentEnv  
 from ray.rllib.agents.ppo import PPOTrainer
+from ray.rllib.agents.dqn import DQNTrainer
 
 from physical_multiagent_env.scenarios.FollowAvoid.scenario import FollowAvoid
 from physical_multiagent_env.reinforcement_learning.utils.observation_functions import Observation_1
@@ -32,9 +34,9 @@ def on_train_result(info):
     training_iteration =  1000
 
     # curriculum : [obstacle, training_iteration]
-    curriculum = [[10], [0], 
-                  [i for i  in range(1, 11)], [100*i for i in range(10)], 
-                  [1, 5, 10], [0, 300, 600]]
+    curriculum = [[[10], [0]],  
+                  [[1, 5, 10], [0, 300, 600]],
+                  [[i for i  in range(1, 11)], [100*i for i in range(10)]]]
 
 
     c = env_config['curriculum_learning']
@@ -77,7 +79,7 @@ if __name__ == '__main__':
             "observation_fn" : Observation_1.observation_fn_1
         },
         'framework' : rllib_config['framework'],
-        #"callbacks":{"on_train_result":on_train_result}
+        "callbacks":{"on_train_result":on_train_result}
     }
 
     if not args.test:
@@ -93,26 +95,39 @@ if __name__ == '__main__':
                             restore= checkpoint if args.resume else None
                     )    
     else:
-        agent = PPOTrainer(config=config, env=FollowAvoidRay)
+        if rllib_config['model'] == "PPO":
+            agent = PPOTrainer(config=config, env=FollowAvoidRay)
+        elif rllib_config['model'] == "DQN":
+            agent = DQNTrainer(config=config, env=FollowAvoidRay)
+        else:
+            raise ValueError()
+
         agent.restore(args.checkpoint)
 
         # config['env_config']['map_size'] = 3
-        config['env_config']["connect"] =p.GUI
+        # config['env_config']["connect"] =p.GUI
 
         env = FollowAvoidRay(config['env_config'])
-        obs = env.reset()
-        done = env.done
-        count = 0  
-        while True:#not done["__all__"]:
-            if count==0:
-                time.sleep(5)
-            alive_agents = [k for k,v in done.items() if v==False]
-            obs = Observation_1.observation_fn_1(obs, env)
-            actions =  {i:agent.compute_action(obs[i], policy_id=f"pol") 
-                                    for i in alive_agents if i!="__all__"}
-            obs, reward, done, info = env.step(actions)
-            time.sleep(0.001)
-            count += 1
+        
+        Reward = [] 
+        for i in range(50):
+            obs = env.reset()
+            done = env.done
+            count = 0
+            Reward.append(0)
+            while not done["__all__"]:
+                # if count==0:
+                #     time.sleep(5)
+                alive_agents = [k for k,v in done.items() if v==False]
+                obs = Observation_1.observation_fn_1(obs, env)
+                actions =  {i:agent.compute_action(obs[i], policy_id=f"pol") 
+                                        for i in alive_agents if i!="__all__"}
+                obs, reward, done, info = env.step(actions)
+                time.sleep(0.001)
+                count += 1
+                Reward[-1] += sum(reward.values())/len(reward.values())
+            print(i,  Reward[-1])
+        print(np.mean(Reward), np.std(Reward))
     
     
     
